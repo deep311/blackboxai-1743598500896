6b68444eb5fbf6d3a72aefd4befe747358394510
deep311 <patildeep603@gmail.com>
2025-04-02 18:24:56 +0530

    Add tailwind.config.js 1743598511249

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#2B6CB0',
+        secondary: '#F8F9FA',
+        accent: '#6C757D'
+      },
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+        heading: ['Poppins', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
